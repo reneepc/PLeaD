06a71f8aed61e8c0433b64d4fc0821d6f7136683
Renê Cardozo <rene.cardozo@opus-software.com.br>
2022-02-07 22:04:13 -0300

    build: create Spring Boot project

---
+++ src/main/java/br/com/opussoftware/plead/PLeaDApplication.java
@@ -0,0 +1,13 @@
+package br.com.opussoftware.plead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PLeaDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PLeaDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/opussoftware/plead/PLeaDApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.opussoftware.plead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PLeaDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
