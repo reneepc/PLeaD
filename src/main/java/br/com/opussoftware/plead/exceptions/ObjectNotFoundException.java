472146be15b3c2051c171c0f7e50fe50518a3396
Renê Cardozo <rene.cardozo@opus-software.com.br>
2022-02-08 23:33:41 -0300

    feat: create ObjectNotFoundException

---
+++ src/main/java/br/com/opussoftware/plead/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package br.com.opussoftware.plead.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(Long id, Class classe) {
+        super("Objeto não encontrado: " + id + ", tipo: " + classe.getSimpleName());
+    }
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
