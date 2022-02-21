ff271554437f8f0998ab816c69c589cf0a69921c
Renê Cardozo <rene.cardozo@opus-software.com.br>
2022-02-21 08:15:27 -0300

    refactor: delete PF and PJ validation from mapper
    The validation is already done by the annotations when saving the prospect.

--- src/main/java/br/com/opussoftware/plead/dtos/mappers/NewProspectDTOMapper.java
+++ src/main/java/br/com/opussoftware/plead/dtos/mappers/NewProspectDTOMapper.java
@@ -4,10 +4,8 @@ import br.com.opussoftware.plead.domain.Prospect;
 import br.com.opussoftware.plead.domain.ProspectPF;
 import br.com.opussoftware.plead.domain.ProspectPJ;
 import br.com.opussoftware.plead.dtos.NewProspectDTO;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public abstract class NewProspectDTOMapper {
@@ -24,21 +22,4 @@ public abstract class NewProspectDTOMapper {
 
     @Mapping(target = "nomeRazaoSocial", source = "razaoSocial")
     public abstract ProspectPJ toProspectPJ(NewProspectDTO newProspectDTO);
-
-    @BeforeMapping
-    private void checkIsValidPF(NewProspectDTO newProspectDTO, @MappingTarget ProspectPF prospectPF) {
-        if(newProspectDTO.getSobrenome() == null ||
-                newProspectDTO.getNome() == null ||
-                newProspectDTO.getCpf() == null) {
-            throw new IllegalArgumentException("O CPF e Sobrenome são obrigatórios para PF");
-        }
-    }
-
-    @BeforeMapping
-    private void checkIsValidPJ(NewProspectDTO newProspectDTO, @MappingTarget ProspectPJ prospectPJ) {
-        if(newProspectDTO.getCnpj() == null ||
-                newProspectDTO.getRazaoSocial() == null) {
-            throw new IllegalArgumentException("O CNPJ é obrigatório para PJ");
-        }
-    }
 }
