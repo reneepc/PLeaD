935230281d883cb23be7a6121c94b6bcfc88d898
Renê Cardozo <rene.cardozo@opus-software.com.br>
2022-02-21 15:23:49 -0300

    feat: add mappers for ProcessoDTO and MidiaNegativaDTO

--- src/main/java/br/com/opussoftware/plead/dtos/mappers/MidiaNegativaDTOMapper.java
+++ src/main/java/br/com/opussoftware/plead/dtos/mappers/MidiaNegativaDTOMapper.java
@@ -1,2 +1,13 @@
-package br.com.opussoftware.plead.dtos.mappers;public interface MidiaNegativaDTOMapper {
+package br.com.opussoftware.plead.dtos.mappers;
+
+import br.com.opussoftware.plead.domain.MidiaNegativa;
+import br.com.opussoftware.plead.dtos.inspecao.MidiaNegativaDTO;
+import org.mapstruct.Mapper;
+
+import java.util.Set;
+
+@Mapper(componentModel = "spring")
+public interface MidiaNegativaDTOMapper {
+    MidiaNegativa toMidiaNegativa(MidiaNegativaDTO midiaNegativaDTO);
+    Set<MidiaNegativa> toMidiaNegativa(Set<MidiaNegativaDTO> midiaNegativaDTO);
 }
--- src/main/java/br/com/opussoftware/plead/dtos/mappers/ProcessoDTOMapper.java
+++ src/main/java/br/com/opussoftware/plead/dtos/mappers/ProcessoDTOMapper.java
@@ -1,2 +1,14 @@
-package br.com.opussoftware.plead.dtos.mappers;public interface ProcessoDTOMapper {
+package br.com.opussoftware.plead.dtos.mappers;
+
+import br.com.opussoftware.plead.domain.Processo;
+import br.com.opussoftware.plead.dtos.inspecao.ProcessoDTO;
+import org.mapstruct.Mapper;
+
+import java.util.Set;
+
+@Mapper(componentModel = "spring")
+public interface ProcessoDTOMapper {
+    Processo toProcesso(ProcessoDTO processoDTO);
+
+    Set<Processo> toProcesso(Set<ProcessoDTO> processoDTO);
 }
