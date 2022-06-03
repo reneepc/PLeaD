2cf4cde91d4a19ba09e9ccd718213fb2cc2e99a1
Renê Cardozo <rene.cardozo@opus-software.com.br>
2022-06-03 13:56:26 -0300

    feat: add CORS to find all prospects

--- backend/src/main/java/br/com/opussoftware/plead/controller/ProspectController.java
+++ backend/src/main/java/br/com/opussoftware/plead/controller/ProspectController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -96,6 +97,7 @@ public class ProspectController {
     }
 
 
+    @CrossOrigin
     @GetMapping
     @ApiOperation(value = "Busca paginação com todos os prospects")
     public ResponseEntity<Page<Prospect>> findAll(
